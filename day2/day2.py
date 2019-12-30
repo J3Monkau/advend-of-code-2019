1b42863e8ce1f5496b42e3873a415aeab521b010
Jack <jmonkau@gmail.com>
2019-12-30 19:05:10 +0100

    Code for part 2 of day 2 and refactor

--- day2/day2.py
+++ day2/day2.py
@@ -1,46 +1,74 @@
 #!/usr/bin/env python3
+from itertools import permutations
 
 INPUT_FILE = "input.txt" 
 
 class IntcodeProgram:
 
 	def __init__(self, intcode):
-		self.intcode = intcode.copy()
-		opcode_indices = range(0, len(self.intcode), 4)
+		self.values = intcode.copy()
+		instr_indices = range(0, len(self.values), 4)
 
-		for idx in opcode_indices:
-			opcode = self.intcode[idx]
+		for idx in instr_indices:
+			instruction = self.values[idx]
 
-			if opcode == 1:
+			if instruction == 1:
 				self._opcode_add(idx)
-			elif opcode == 2:
+			elif instruction == 2:
 				self._opcode_mul(idx)
-			elif opcode == 99:
+			elif instruction == 99:
 				break
 
+	@property
+	def output(self):
+		return self.values[0]
+
 	def _opcode_add(self, pos):
-		left, right, dest = self.intcode[pos+1:pos+4]
-		self.intcode[dest] = self.intcode[left] + self.intcode[right]
+		_, noun_addr, verb_addr, dest_addr = self.values[pos:pos+4]
+
+		self.values[dest_addr] = \
+				self.values[noun_addr] + \
+				self.values[verb_addr]
 	
 	def _opcode_mul(self, pos):
-		left, right, dest = self.intcode[pos+1:pos+4]
-		self.intcode[dest] = self.intcode[left] * self.intcode[right]
+		_, noun_addr, verb_addr, dest_addr = self.values[pos:pos+4]
 
+		self.values[dest_addr] = \
+				self.values[noun_addr] * \
+				self.values[verb_addr]
+	
 	def __getitem__(self, key):
-		return self.intcode[key]
+		return self.values[key]
 
 	def __str__(self):
-		return str(self.intcode)
+		return str(self.values)
 
 if __name__ == "__main__":
 
 	with open(INPUT_FILE) as file:
 		code = list(map(int, file.readline().split(',')))
 
-		# restore "1202 program alarm" state
-		code[1] = 12
-		code[2] = 2
+	print("---- DAY 2 ----")
 
-		program = IntcodeProgram(code)
+	# --------- Part 1 ---------
+	code_part1 = code.copy()
 
-		print(program[0])
+	code_part1[1:3] = 12, 2	# restore "1202 program alarm" state
+	program_part1 = IntcodeProgram(code_part1)
+	print("Answer part 1: %s" % program_part1.output)
+
+	# --------- Part 2 ---------
+	PART2_PROGRAM_OUTPUT = 19690720
+
+	code_part2 = code.copy()
+	search_range = range(len(code_part2))
+
+	# search for params at 1 and 2 that produce the target ouput
+	for verb, noun in permutations(search_range, r=2):
+			code_part2[1:3] = verb, noun  # setting params
+			program = IntcodeProgram(code_part2)
+			if program.output == PART2_PROGRAM_OUTPUT:
+				print("Answer part 2: verb=%s, noun=%s" % (verb, noun))
+				exit(0)
+
+	print("Failed to find noun and verb that cause the program to produce output %s" % PART2_PROGRAM_OUTPUT)
